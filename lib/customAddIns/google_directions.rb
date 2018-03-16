e881dfcb4d8bdda42a0d50cdc3dae130aa830e48
ChrissyMac <11790059+chri55y@users.noreply.github.com>
2018-03-16 13:47:47 -0500

    file path fix for Config

--- lib/customAddIns/google_directions.rb
+++ lib/customAddIns/google_directions.rb
@@ -1,4 +1,4 @@
-$:.unshift File.expand_path('../lib/', __dir__)
+# $:.unshift File.expand_path('../lib/', __dir__)
 
 require 'active_support'
 require 'active_support/core_ext'
@@ -13,7 +13,13 @@ module GoogleDirections
       block.call(self.config)
     end
 
-    autoload :Config  , 'google_directions/config'
+    # autoload :Config  , 'google_directions/config'
+    # autoload :Error   , 'google_directions/error'
+    # autoload :Encoder , 'google_directions/encoder'
+    # autoload :Request , 'google_directions/request'
+    # autoload :Version , 'google_directions/version'
+
+    autoload :Config  , __dir__+'/google_directions/config'
     autoload :Error   , 'google_directions/error'
     autoload :Encoder , 'google_directions/encoder'
     autoload :Request , 'google_directions/request'
