11fa35668d5c61fe410d7e9d756cdcba6328c6d3
ChrissyMac <11790059+chri55y@users.noreply.github.com>
2018-03-16 11:24:18 -0500

    setting up files to initialize GoogleDirections Module
    google_directions.rb contents commented out for now and added to z01 to
    see if this will work without the extra file jump. at this point, rails
    server starts with no errors, but we haven’t called GoogleDirections or
    any classes

--- config/initializers/z01_initsANDfilestructures.rb
+++ config/initializers/z01_initsANDfilestructures.rb
@@ -1,1 +1,33 @@
-# puts "in file #{__FILE__}"
+puts "/n  start file #{File.basename(__FILE__)}"
+
+# autoload(GoogleDirections, '../../lib/customAddIns/google_directions.rb')
+#
+
+# r = Request.new
+
+
+# $:.unshift File.expand_path('../lib/', __dir__)
+
+
+require 'active_support'
+require 'active_support/core_ext'
+require 'json'
+
+module GoogleDirections
+  def self.config
+    Config.instance
+  end
+
+  def self.configure(&block)
+    block.call(self.config)
+  end
+
+  autoload :Config  , '../../lib/customAddIns/google_directions/config'
+  autoload :Error   , '../../lib/customAddIns/google_directions/error'
+  autoload :Encoder , '../../lib/customAddIns/google_directions/encoder'
+  autoload :Request , '../../lib/customAddIns/google_directions/request'
+  autoload :Version , '../../lib/customAddIns/google_directions/version'
+
+end
+
+
---
+++ lib/customAddIns/google_directions.rb
@@ -0,0 +1,22 @@
+# $:.unshift File.expand_path('../lib/', __dir__)
+#
+# require 'active_support'
+# require 'active_support/core_ext'
+# require 'json'
+#
+# module GoogleDirections
+#     def self.config
+#       Config.instance
+#     end
+#
+#     def self.configure(&block)
+#       block.call(self.config)
+#     end
+#
+#     autoload :Config  , 'google_directions/config'
+#     autoload :Error   , 'google_directions/error'
+#     autoload :Encoder , 'google_directions/encoder'
+#     autoload :Request , 'google_directions/request'
+#     autoload :Version , 'google_directions/version'
+#
+# end
