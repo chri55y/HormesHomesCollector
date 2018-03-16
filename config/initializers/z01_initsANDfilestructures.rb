d54c4f8e3865e465465aa3e76a4abfe81e25415c
ChrissyMac <11790059+chri55y@users.noreply.github.com>
2018-03-16 12:24:47 -0500

    solution: use .config.private_key, not class method ::Config.private_key
    New Error: loading request file

--- config/initializers/z01_initsANDfilestructures.rb
+++ config/initializers/z01_initsANDfilestructures.rb
@@ -38,16 +38,17 @@ Rails.configuration.google = {:api_key => ENV['GOOGLE_MAPS_API_KEY']}
 
 
 puts "\taccess files in #{File.expand_path("../../lib/customAddIns/google_directions/config", __dir__)}"
-GoogleDirections::Config.private_key=Rails.configuration.google[:api_key]
+# GoogleDirections::Config.private_key=Rails.configuration.google[:api_key]
 # ERROR => /Users/Chrissy/.rvm/gems/ruby-2.4.1/gems/activesupport-5.1.5/lib/active_support/dependencies.rb:292:in `require': cannot load such file -- ../../lib/customAddIns/google_directions/config (LoadError)
 # failed solutions:
 #     > adding .rb file extension => autoload :Config  , '../../lib/customAddIns/google_directions/config.rb'
 # solution: use File.expand_path (line 27)
-
+GoogleDirections.config.private_key=Rails.configuration.google[:api_key]
 # NEW ERROR => /Users/Chrissy/develop/realestate/HormesHomesCollector/config/initializers/z01_initsANDfilestructures.rb:41:in `<top (required)>': undefined method `private_key=' for GoogleDirections::Config:Class (NoMethodError)
+# solution: use .config.private_key, not class method ::Config.private_key
 
 
-
+puts ""
 req = GoogleDirections::Request.new
 
 
