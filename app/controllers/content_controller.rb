403e3b50cd99a11cb94eec3d8605d21cba1b3218
ChrissyMac <11790059+chri55y@users.noreply.github.com>
2018-03-17 18:48:49 -0500

    confirmed: we can make a GoogleDirections Request w/o re-configuring api key. | Note: I do load the GoogleDirections module within ContentController#home

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -2,6 +2,17 @@ class ContentController < ApplicationController
 
   def home
 
+    autoload :GoogleDirections, File.expand_path('../../lib/customAddIns/google_directions.rb',__dir__)
+
+    @req = GoogleDirections::Request.new
+    @test_params = { :origin      => "Disneyland" ,
+                    :destination => "Universal Studios Hollywood"
+    }
+    @response = @req.get(@test_params)
+    # puts @response.inspect
+
   end
+
+
 
 end
