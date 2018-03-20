f6b8cc5a9e53972a49373ae865c6f900190c0cc1
ChrissyMac <11790059+chri55y@users.noreply.github.com>
2018-03-20 17:07:25 -0500

    test configuration by making a test call. currently test fails: Error: invalid or missing ZWSID parameter

--- config/initializers/z02_rubillow.rb
+++ config/initializers/z02_rubillow.rb
@@ -6,6 +6,14 @@ Rubillow.configure do |configuration|
   configuration.zwsid = ENV['ZWSID']
 end
 
+puts "\t> make test API call"
+test_property = Rubillow::HomeValuation.zestimate({ :zpid => '48749425' })
+if test_property.success?
+  puts "\t\tSuccess! price is $#{test_property.price}"
+else
+  puts "\t\tFAIL---------------------------"
+  puts test_property.inspect
+end
 
 puts "\n   end file #{File.basename(__FILE__)}\n"
 
