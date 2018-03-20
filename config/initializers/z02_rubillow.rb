8ae1dfadd0786bcb761c1c3c8e713a8aa46afff6
ChrissyMac <11790059+chri55y@users.noreply.github.com>
2018-03-20 16:59:52 -0500

    configure ZWSID for Rubillow (also added ZWSID to .bash_profile)

--- config/initializers/z02_rubillow.rb
+++ config/initializers/z02_rubillow.rb
@@ -1,6 +1,10 @@
-puts "=> start file #{File.basename(__FILE__)}"
+puts "\n=> start file #{File.basename(__FILE__)}"
 
-
+puts "\t> configuring zwsid"
+# from https://github.com/synewaves/rubillow
+Rubillow.configure do |configuration|
+  configuration.zwsid = ENV['ZWSID']
+end
 
 
 puts "\n   end file #{File.basename(__FILE__)}\n"
